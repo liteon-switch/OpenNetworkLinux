fa31e2df600fdde2d166c533466ef7a64c5a72c1
debian <debian@debian.hanly>
2017-05-24 11:26:53 +0800

    add liteon-switch TGSM602-NF platform support

---
+++ packages/platforms/liteon/x86-64/x86-64-liteon-tgsm602-54x/onlp/builds/src/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_liteon_tgsm602_54x Autogeneration
+#
+###############################################################################
+x86_64_liteon_tgsm602_54x_AUTO_DEFS := module/auto/x86_64_liteon_tgsm602_54x.yml
+x86_64_liteon_tgsm602_54x_AUTO_DIRS := module/inc/x86_64_liteon_tgsm602_54x module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/liteon/x86-64/x86-64-liteon-tgsm602-54x/platform-config/r0/src/python/x86_64_liteon_tgsm602_54x_r0/__init__.py
@@ -0,0 +1,74 @@
+from onl.platform.base import *
+from onl.platform.liteon import *
+
+class OnlPlatform_x86_64_liteon_tgsm602_54x_r0(OnlPlatformLiteon,
+                                              OnlPlatformPortConfig_48x10_6x40):
+    PLATFORM='x86-64-liteon-tgsm602-54x-r0'
+    MODEL="TGSM602-54X"
+    SYS_OBJECT_ID=".6000.64.1"
+
+    def baseconfig(self):
+
+        self.insmod('cpr_4011_4mxx')
+        self.insmod('liteon_i2c_cpld')
+        self.insmod_platform()
+
+        ########### initialize I2C bus 0 ###########
+
+        # initialize multiplexer (PCA9548)
+        self.new_i2c_devices(
+            [
+                ('pca9548', 0x71, 0),
+                ('pca9548', 0x72, 0),
+                ('pca9548', 0x73, 0),
+                ('pca9548', 0x74, 0),
+                ('pca9548', 0x75, 0),
+                ('pca9548', 0x76, 0),
+                ('pca9548', 0x77, 0),
+                ]
+            )
+        # initialize CPLDs
+        self.new_i2c_devices(
+            [
+                ('liteon_i2c_cpld', 0x60, 0),
+                ('liteon_i2c_cpld', 0x61, 0),
+                ('liteon_i2c_cpld', 0x62, 0),
+                ]
+            )
+        # initialize SFP devices
+        for port in range(1, 49):
+            self.new_i2c_device('sfp%d' % port, 0x50, port+1)
+            self.new_i2c_device('sfp%d' % port, 0x51, port+1)
+
+        # Initialize QSFP devices
+        self.new_i2c_device('sfp51', 0x50, 50)
+        self.new_i2c_device('sfp54', 0x50, 51)
+        self.new_i2c_device('sfp50', 0x50, 52)
+        self.new_i2c_device('sfp53', 0x50, 53)
+        self.new_i2c_device('sfp49', 0x50, 54)
+        self.new_i2c_device('sfp52', 0x50, 55)
+
+        ########### initialize I2C bus 1 ###########
+        self.new_i2c_devices(
+            [
+                # initiate multiplexer (PCA9548)
+                ('pca9548', 0x70, 1),
+
+                # initiate PSU-1 AC Power
+                ('tgsm602_54x_psu', 0x38, 59),
+                ('cpr_4011_4mxx',  0x3c, 59),
+
+                # initiate PSU-2 AC Power
+                ('tgsm602_54x_psu', 0x3b, 60),
+                ('cpr_4011_4mxx',  0x3f, 60),
+
+                # initiate lm75
+                ('lm75', 0x48, 63),
+                ('lm75', 0x49, 64),
+                ('lm75', 0x4a, 65),
+
+                # initiate IDPROM
+                ('24c02', 0x57, 1),
+                ]
+            )
+        return True
